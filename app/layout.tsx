9c24c7bfaaa6089d0cfb1e3edd03fa4973023395
Shaikhs077 <s48541324@gmail.com>
2025-05-20 15:35:29 +0530

    first commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,12 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Rethink_Sans } from 'next/font/google'
+ 
+// If loading a variable font, you don't need to specify the font weight
+const inter = Rethink_Sans({
+  subsets: ['latin'],
+  display: 'swap',
+})
 import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -25,7 +21,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+       className={inter.className}
       >
         {children}
       </body>
